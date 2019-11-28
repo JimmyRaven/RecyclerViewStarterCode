0e510b5e233ee2110425a7e79e62e34b37800aa8
JimmyRaven <chunxiluo@yandex.com>
2019-11-28 08:10:06 +0800

    Done

--- app/src/main/java/com/example/recyclerviewstudentversion/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewstudentversion/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.recyclerviewstudentversion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 // Todo create a player class that will hold info about the player
@@ -24,22 +30,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView=(RecyclerView)findViewById(R.id.recyclerview);
+        list = getPlayers();
+        mAdapter = new MyRecyclerAdapter(list);
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
 
     }
     //Todo create method that will fill list of players
-    public List getPlayers()
-    {
-        List<Player> list = Arrays.asList(new Player[]{
-                new Player("Lionel Messi",32,44224400.00,"football",R.drawable.mesi),
-                new Player("David Robert Joseph Beckham",44,1291800.00,"football",R.drawable.beckham),
-                new Player("Hanyu Yuzulu",24,500000,"figure skating",R.drawable.hanyu),
-                new Player("Zhang Jike",31,500000,"Ping-pong",R.drawable.zhang),
-                new Player("Lin Dan",36,10000000,"Badminton",R.drawable.lin),
-                new Player("Lee Chong Wei",37,5000000,"Badminton",R.drawable.lee),
-                new Player("Lee Sang-hyeok (Faker)",23,890000,"E-sports (LOL)",R.drawable.faker),
-                new Player("Fu Yuanhui",23,5000000,"Swimming",R.drawable.fu),
-                new Player("Lang Ping",58,5000000,"Volleyball",R.drawable.lang)
-        });
-        return list;
+    public ArrayList getPlayers() {
+        ArrayList<Player> players = new ArrayList<Player>();
+        players.add(new Player("Cristiano Ronaldo", 34, 460000000, "football", R.drawable.cronaldo, "https://en.wikipedia.org/wiki/Cristiano_Ronaldo"));
+        players.add(new Player("LeBron James", 34, 480000000, "basketball", R.drawable.ljames, "https://en.wikipedia.org/wiki/LeBron_James"));
+        players.add(new Player("Lionel Messi", 32, 400000000, "football", R.drawable.lmessi, "https://en.wikipedia.org/wiki/LeBron_James"));
+        players.add(new Player("Sun Yang", 27, 3000000, "swimming", R.drawable.syang, "https://en.wikipedia.org/wiki/Sun_Yang"));
+        players.add(new Player("Usain Bolt", 33, 90000000, "track and field", R.drawable.ubolt, "https://en.wikipedia.org/wiki/Sun_Yang"));
+        players.add(new Player("Zhang Jike", 31, 1000000, "table tennis", R.drawable.zjike, "https://en.wikipedia.org/wiki/Zhang_Jike"));
+        players.add(new Player("Serena Williams", 38, 200000000, "tennis", R.drawable.swilliams, "https://en.wikipedia.org/wiki/Serena_Williams"));
+        players.add(new Player("Li Na", 37, 50000000, "tennis", R.drawable.lna, "https://en.wikipedia.org/wiki/Li_Na"));
+        // Phillip, add another 7
+        players.add(new Player("Derrick Rose", 31, 2100000, "basketball", R.drawable.drose, "https://en.wikipedia.org/wiki/Derrick_Rose"));
+        players.add(new Player("Aaron Rodgers", 35, 89300000, "American football", R.drawable.arodgers, "https://en.wikipedia.org/wiki/Aaron_Rodgers"));
+        players.add(new Player("Canelo Alvarez", 29, 92000000, "boxing", R.drawable.calvarez, "https://en.wikipedia.org/wiki/Canelo_%C3%81lvarez"));
+        players.add(new Player("Mike Trout", 28, 50600000, "baseball", R.drawable.mtrout, "https://en.wikipedia.org/wiki/Mike_Trout"));
+        players.add(new Player("Conor McGregor", 31, 47000000, "MMA", R.drawable.cmcgregor, "https://en.wikipedia.org/wiki/Conor_McGregor"));
+        players.add(new Player("Stephen Curry", 31, 79800000, "basketball", R.drawable.curry, "https://en.wikipedia.org/wiki/Stephen_Curry"));
+        players.add(new Player("Chris Paul", 34, 43800000, "basketball", R.drawable.cp, "https://en.wikipedia.org/wiki/Chris_Paul"));
+        return players;
     }
 }
--- app/src/main/java/com/example/recyclerviewstudentversion/MyRecyclerAdapter.java
+++ app/src/main/java/com/example/recyclerviewstudentversion/MyRecyclerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.recyclerviewstudentversion;
 
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,15 +21,17 @@ import java.util.List;
 public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.MyViewHolder> {
 
     private List<Player> mPlayer;
+    Context context;
 
     public MyRecyclerAdapter(List<Player> mPlayer) {
         this.mPlayer = mPlayer;
     }
 
+
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int   viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.single_view,parent,false);
+                .inflate(R.layout.single_item_view,parent,false);
 
         return new MyViewHolder(itemView);
     }
@@ -37,12 +40,16 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.My
     public void onBindViewHolder(MyViewHolder holder, final int position) {
         Player playerlist = mPlayer.get(position);
         holder.nameText.setText(playerlist.getName());
-        holder.ageText.setText(playerlist.getAge());
-        holder.worthText.setText(String.valueOf(playerlist.getMoney()));
+        holder.ageText.setText("Age: " + String.valueOf(playerlist.getAge()));
+        holder.worthText.setText("Net Worth: $" + String.valueOf(playerlist.getMoney()));
         holder.sportText.setText(playerlist.getSport());
         holder.pic.setImageResource(playerlist.getImage());
 
 
+    }
+
+    @Override public int getItemCount() {
+        return mPlayer.size();
     }
 
 
@@ -56,11 +63,12 @@ public static class MyViewHolder extends RecyclerView.ViewHolder {
     TextView nameText, ageText, worthText, sportText;
 
     private MyViewHolder(@NonNull View itemView) {
-        nameText = itemView.findViewbyId(R.id.name);
-        ageText = itemView.findViewbyId(R.id.age);
-        worthText = itemView.findViewbyId(R.id.money);
-        sportText = itemView.findViewbyId(R.id.sport);
-        pic = itemView.findViewbyId(R.id.pic);
+        super(itemView);
+        nameText = itemView.findViewById(R.id.name);
+        ageText = itemView.findViewById(R.id.age);
+        worthText = itemView.findViewById(R.id.money);
+        sportText = itemView.findViewById(R.id.sport);
+        pic = itemView.findViewById(R.id.image);
 
     }
 }
